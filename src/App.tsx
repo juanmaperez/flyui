0d0f56e7fffd9b9ac6309ce6acc7d18d0f5c8835
Juanma Perez <juanmaperezvar@gmail>
2022-07-18 23:59:23 +0100

    initial commit and clean up

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <h1>FlyUI</h1>
+      </header>
+
+      <main>
+
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
